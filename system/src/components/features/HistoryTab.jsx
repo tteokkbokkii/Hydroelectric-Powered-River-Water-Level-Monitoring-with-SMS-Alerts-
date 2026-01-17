08a03b1194b7e28501ac85cd69e1ef0470db6fb1
Eve <aliyahjessey.delacruz@gmail.com>
2026-01-17 21:46:10 +0800

    fix column in tab

--- system/src/components/features/HistoryTab.jsx
+++ system/src/components/features/HistoryTab.jsx
@@ -17,19 +17,33 @@ const HistoryTab = () => {
             className={`nav-item ${activeTab === 'PREDICTION' ? 'is-active' : ''}`}
             onClick={() => setActiveTab('PREDICTION')}
           >
-            PREDICTION
+            PREDICTED READING
           </button>
         </div>
 
         <div className="tab-panel">
           {activeTab === 'ACTUAL READING' && (
             <div className="panel-content-area" id="ACTUAL READING-section">
-              <p className='date'>DATE:</p>
+              
+              <div className="columns-container">
+                
+                {/* PANEL 1 */}
+                <div className="content-column column-1">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, laboriosam soluta! Voluptatibus dicta est accusamus fugit architecto qui aliquam cum. Voluptate possimus aut vel pariatur optio minus soluta velit ex.</p>
+                </div>
+
+                {/* PANEL 2 */}
+                <div className="content-column column-2">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente harum, maxime aliquid provident amet dicta consectetur ex incidunt nobis cupiditate quis at fugiat. Perspiciatis id sint eius dignissimos impedit eveniet.</p>
+                </div>
+
+              </div>
             </div>
           )}
 
           {activeTab === 'PREDICTION' && (
             <div className="panel-content-area" id="PREDICTION-section">
+              <p className='date'>PREDICTION VIEW</p>
             </div>
           )}
         </div>
