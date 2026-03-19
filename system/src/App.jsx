2bfc627f5a9fa2914a6c6578a3f781ddddb3af8d
Eve <aliyahjessey.delacruz@gmail.com>
2026-03-20 02:37:05 +0800

    handshake

--- system/src/App.jsx
+++ system/src/App.jsx
@@ -29,7 +29,6 @@ function App() {
             </Router>
             */}
 
-
             <HandshakeCheck />
         </>
     ); // Added the missing semicolon and ensured the brace is closed below
