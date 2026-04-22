909a1f94943cb0b28925469433f847923c860a88
Eve <aliyahjessey.delacruz@gmail.com>
2026-04-22 11:14:03 +0800

    fix case senstivity

--- system/src/App.jsx
+++ system/src/App.jsx
@@ -12,7 +12,7 @@ import Dashboard from './pages/Dashboard'
 import History from './pages/History'
 import System from './pages/System'
 import HandshakeCheck from './components/HandshakeCheck.jsx';
-import TestPage from './pages/testpage.jsx';
+import TestPage from './pages/Testpage.jsx';
 
 
 function App() {
