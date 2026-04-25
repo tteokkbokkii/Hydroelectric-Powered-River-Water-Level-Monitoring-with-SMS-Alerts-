4e122288a362e80626857e8699923b7389550905
JuanBRosa <johnrielbarrosa@gmail.com>
2026-04-25 17:10:31 +0800

    jsx connections

--- system/src/main.jsx
+++ system/src/main.jsx
@@ -8,7 +8,7 @@ import './styles/Card.css'
 import './styles/SystemTab.css'
 import './styles/HistoryTab.css'
 import './styles/ContactsTab.css'
-import './styles/Popup.css' // <-- ADD THIS LINE
+import './features/Popup.css' // <-- ADD THIS LINE
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
