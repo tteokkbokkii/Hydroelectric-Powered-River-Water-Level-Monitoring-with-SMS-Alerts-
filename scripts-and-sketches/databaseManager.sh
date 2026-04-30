e8968f90894cd5cfa2f37261e41a0eab55e69cea
Eve <aliyahjessey.delacruz@gmail.com>
2026-04-30 12:11:23 +0800

    fix mount name

--- scripts-and-sketches/databaseManager.sh
+++ scripts-and-sketches/databaseManager.sh
@@ -4,7 +4,7 @@
 DB_DIR="/home/admin/thesis/server"
 DB_NAME="river_monitor.db"
 DB_PATH="$DB_DIR/$DB_NAME"
-USB_MOUNT="/media/admin/MULTIBOOT"
+USB_MOUNT="/media/admin/USB-BACKUP"
 USB_DEST="$USB_MOUNT/thesis_backups"
 MAIN_LOG="/home/admin/thesis/maintenance.log"
 
